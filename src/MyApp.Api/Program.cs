82811ca2629d4eb27131ac0278aaf7cbb36c8dfe
Niveditha.VS <niveditha.vs@mclarensv.com>
2026-07-31 14:41:31 +0530

    qa testing

--- src/MyApp.Api/Program.cs
+++ src/MyApp.Api/Program.cs
@@ -1,1 +1,1 @@
-var app=WebApplication.CreateBuilder(args).Build(); app.MapGet("/",()=>"Hello from MyApp qa"); app.Run();
+var app=WebApplication.CreateBuilder(args).Build(); app.MapGet("/",()=>"Hello from MyApp qa testing"); app.Run();
