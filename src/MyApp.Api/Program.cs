7f04c61c622941ace648c9d0fba6b438d51d6700
niveditha <niveditha.job11@gmail.com>
2026-08-03 17:37:18 +0530

    testing 2nd

--- src/MyApp.Api/Program.cs
+++ src/MyApp.Api/Program.cs
@@ -1,1 +1,1 @@
-var app=WebApplication.CreateBuilder(args).Build(); app.MapGet("/",()=>"Hello from MyApp"); app.Run();
+var app=WebApplication.CreateBuilder(args).Build(); app.MapGet("/",()=>"Hello from MyApp testing test"); app.Run();
